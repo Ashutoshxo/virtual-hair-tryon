9ca21e8e825535b8f1cfc55a6dc84ddcb672d97d
Ashutosh S Gautam <ashutoshxo@users.noreply.github.com>
2025-11-05 12:18:57 +0530

    Fix MySQL connection variables

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,14 @@ require('dotenv').config();
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://virtual-hair-tryon-rfkwxy824-ashutoshs-projects-b06ecb4b.vercel.app',
+    'https://virtual-hair-tryon.vercel.app/' 
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
